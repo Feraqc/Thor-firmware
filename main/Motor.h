ea592e32c175e165513a9891d958c2f584802871
Feraqc <aqnc.fe@gmail.com>
2024-05-13 11:23:31 -0400

    added second motor attribute to articulation class

--- main/Motor.h
+++ main/Motor.h
@@ -5,82 +5,155 @@
 
 class Articulacion{
     public:
-      AccelStepper motor;
-      int step_pin;
-      int dir_pin;
-      int sensor_pin;
+      AccelStepper motor1;
+      AccelStepper motor2;
+      int stepPin1;
+      int dirPin1;
+      int stepPin2;
+      int dirPin2;
+      int sensorPin;
       float steps_per_unit;
-      float min_pos;
-      float max_pos;
-      float max_speed;
+      float minPos;
+      float maxPos;
+      float maxSpeed;
       int offSet;
       int currentPosition;
 
+// CONSTRUCTOR UN MOTOR
 Articulacion(
-    int _dir_pin,
-    int _step_pin,
-    int _sensor_pin,
-    float _steps_per_unit
-    // const float _min_pos,
-    // const float _max_pos,
-    // const float _max_speed
-) : motor(AccelStepper(AccelStepper::DRIVER, _step_pin, _dir_pin)) {
-    step_pin = _step_pin;
-    dir_pin = _dir_pin;
+    int _dirPin1,
+    int _stepPin1,
+    int _sensorPin,
+    float _steps_per_unit): 
+    motor1(AccelStepper(AccelStepper::DRIVER, _stepPin1, _dirPin1)){
+    stepPin1 = _stepPin1;
+    dirPin1 = _dirPin1;
+    stepPin2 = -1;
+    dirPin2 = -1;
     steps_per_unit = _steps_per_unit;
-    pinMode(_sensor_pin, INPUT);
-    // min_pos = _min_pos;
-    // max_pos = _max_pos;
-    max_speed = 1000;
-    sensor_pin = _sensor_pin;
-    motor.setMaxSpeed(30000.0);
-    motor.setAcceleration(100.0);
-};
+    pinMode(_sensorPin, INPUT);
+    maxSpeed = 1000;
+    sensorPin = _sensorPin;
+    motor1.setMaxSpeed(1000.0);
+    motor1.setAcceleration(250.0);
+}
+
+// CONSTRUCTOR DOS MOTORES
+Articulacion(
+    int _dirPin1,
+    int _stepPin1,
+    int _dirPin2,
+    int _stepPin2,
+    int _sensorPin,
+    float _steps_per_unit
+) : motor1(AccelStepper(AccelStepper::DRIVER, _stepPin1, _dirPin1)),
+    motor2(AccelStepper(AccelStepper::DRIVER, _stepPin2, _dirPin2)){
+    stepPin1 = _stepPin1;
+    dirPin1 = _dirPin1;
+    stepPin2 = _stepPin2;
+    dirPin2 = _dirPin2;
+    steps_per_unit = _steps_per_unit;
+    pinMode(_sensorPin, INPUT);
+    maxSpeed = 1000;
+    sensorPin = _sensorPin;
+    motor1.setMaxSpeed(30000.0);
+    motor1.setAcceleration(50.0);
+    motor2.setMaxSpeed(30000.0);
+    motor2.setAcceleration(10.0);
+}
 
 
     void setOffset(){
+      Serial.println("Setting offSet");
       bool offSetFlag = false;
+      moveTo(360,10);
       while(!offSetFlag){
-        motor.moveTo((int)(360*steps_per_unit));
-        motor.setSpeed(50*steps_per_unit);
-        motor.runSpeedToPosition();
-        offSetFlag = !digitalRead(sensor_pin);
+        motor1.runSpeedToPosition();
+        if(stepPin2 != -1 && dirPin2 != -1){
+          motor2.runSpeedToPosition();
+        }
+        offSetFlag = !digitalRead(sensorPin);
       }
-      motor.stop();
-      motor.setSpeed(0);
-      offSet = motor.currentPosition();
+      motor1.stop();
+      motor1.setSpeed(0);
+      if(stepPin2 != -1 && dirPin2 != -1){
+        motor2.stop();
+        motor2.setSpeed(0);
+      }
+      offSet = motor1.currentPosition();
+      maxPos = motor1.currentPosition();
+
+      if(stepPin2 != -1 && dirPin2 != -1){
+        bool offSetFlag2 = false;
+        moveTo(-2*360,10);
+        while(!offSetFlag2){
+          static unsigned long secondOffset = millis();
+          motor1.runSpeedToPosition();
+          motor2.runSpeedToPosition();
+          if(millis() - secondOffset >= 2000){
+            offSetFlag2 = !digitalRead(sensorPin);
+          }
+        }
+        motor1.stop();
+        motor1.setSpeed(0);
+        if(stepPin2 != -1 && dirPin2 != -1){
+          motor2.stop();
+          motor2.setSpeed(0);
+        }
+          minPos = motor1.currentPosition();
+      }
+
+      Serial.print(maxPos);
+      Serial.print(",");
+      Serial.println(minPos);
+
+      Serial.println("End offSet");
     }
   
     void setSpeed(float motor_speed){
-      motor.setMaxSpeed(motor_speed * steps_per_unit);
-    };
-    void setAccel(float motor_accel){
-      motor.setAcceleration(motor_accel*steps_per_unit);
+      motor1.setMaxSpeed(motor_speed * steps_per_unit);
+      if(stepPin2 != -1 && dirPin2 != -1){
+        motor2.setMaxSpeed(motor_speed * steps_per_unit);
+      }
     };
 
+    void setAccel(float motor_accel){
+      motor1.setAcceleration(motor_accel*steps_per_unit);
+      if(stepPin2 != -1 && dirPin2 != -1){
+        motor2.setAcceleration(motor_accel*steps_per_unit);
+      }
+    };
+
+    // falta offset
     void moveTo(float move_target,float speed_target){
       int target = (int)(move_target*steps_per_unit);
-      motor.moveTo(target + offSet);
-      motor.setMaxSpeed(speed_target*steps_per_unit);
-      motor.setSpeed(speed_target*steps_per_unit);
+      motor1.moveTo(target);
+      //motor1.setMaxSpeed(speed_target*steps_per_unit);
+      motor1.setSpeed(speed_target*steps_per_unit);
+      if(stepPin2 != -1 && dirPin2 != -1){
+        motor2.setSpeed(speed_target*steps_per_unit);
+      }
     };
 
     unsigned long calibrate(float positionTarget, float speedTarget){
-      moveTo(positionTarget, speedTarget);
+      moveTo(positionTarget,speedTarget);
+      unsigned long serialTime = millis();
       unsigned long startTime = millis();
-      unsigned long lastSerialPrintTime = 0;
-    
-    while (true) {
-        motor.runSpeedToPosition();
-        if (motor.currentPosition() == floor(abs(positionTarget)*steps_per_unit)) {
-            return (millis()-startTime);
+      while (true) {
+       // motor1.run();
+     motor1.runSpeedToPosition();
+        // if(millis()-serialTime >= 500){
+        //   serialTime = millis();
+        //   Serial.println(motor1.currentPosition());
+        // }
+        if (abs(motor1.currentPosition()) == floor(abs(positionTarget)*steps_per_unit)+offSet) {
+          return (millis()-startTime);
             break;
         }
-        if(millis()-startTime > 10000){
+        if(millis()-startTime > 25000){
           break;
         }
-    }
-    Serial.println("Calibration complete");
+      }
     }
 
 
--- main/main.ino
+++ main/main.ino
@@ -1,39 +1,65 @@
 #include "Motor.h"
 
-#define dirPin1 36
-#define stepPin1 28
-#define sensorPin1 43
+// Articulacion 1
+#define dirPinA1 5
+#define stepPinA1 2
+#define sensorPin1 9
 #define stepsPerUnit1 2.7777
+
+// // Articulacion 2
+// #define dirPin2A 34
+// #define stepPin2A 26
+// #define dirPin2B 32
+// #define stepPin2B 24
+// #define sensorPin2 44
+// #define stepsPerUnit2 236.8
+
+
 
 float position;
 float speed;
 unsigned long sum1=0;
 unsigned long sum2=0;
-Articulacion articulacion1(dirPin1,stepPin1,sensorPin1,stepsPerUnit1);
+Articulacion articulacion1(dirPinA1,stepPinA1,sensorPin1,stepsPerUnit1);
+//Articulacion articulacion2(dirPin2A,stepPin2A,dirPin2B,stepPin2B,sensorPin2,stepsPerUnit2);
+
 
 void setup() {
   Serial.begin(9600);
-  articulacion1.setOffset();
+  //articulacion1.setOffset();
+  //articulacion2.setOffset();
+  pinMode(8,OUTPUT);
+  digitalWrite(8,LOW);
+  pinMode(dirPinA1,OUTPUT);
+  pinMode(stepPinA1,OUTPUT);
 
+  articulacion1.offSet = articulacion1.motor1.currentPosition();
+  Serial.println(articulacion1.offSet);
 }
 
 void loop() {
+
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
     parseCommand(command);
     Serial.print(position);
     Serial.print(",");
     Serial.println(speed);
-    //articulacion1.moveTo(position,speed);
-    for(int i=0;i<5;i++){
-      sum1 = sum1 + articulacion1.calibrate(360,speed);
-      sum2 = sum2 + articulacion1.calibrate(0,speed);
+    //articulacion1.moveTo(position, speed);
+    //articulacion2.moveTo(position, speed);
+    sum1 = 0;
+    for(int i=0;i<1;i++){
+      sum1 = articulacion1.calibrate(position,speed);
+      Serial.println("end");
     }
-    Serial.println(sum1/5);
-    Serial.println(sum2/5);
+    Serial.print(sum1);
+    sum1 = 0;
   }
-  //articulacion1.motor.runSpeedToPosition();
+    //articulacion1.motor1.runSpeedToPosition();
+    //articulacion2.motor1.runSpeedToPosition();
+    //articulacion1.motor1.runSpeedToPosition();
   
+
 }
 
 void parseCommand(String cmd) {
